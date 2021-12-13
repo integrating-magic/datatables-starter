752bbbd35472545108a4fdef3ae68cc5cd7b02ae
Jeremy Brown <jeremy@integratingmagic.io>
2021-12-12 20:09:27 -0500

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,11 @@
+//Here we're importing items we'll need. You can add other imports here.
+import "./style.css";
+import "regenerator-runtime/runtime.js";
+
+//The first function. Remove this.
+const btn = document.querySelector("button");
+btn.onclick = function () {
+  alert("You ran some JavaScript");
+};
+
+//
