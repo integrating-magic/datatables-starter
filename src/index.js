136508baf7cfcf8e0d92cb4001db29b21427bab2
Jeremy Brown <jlbmagic@gmail.com>
2022-03-30 08:47:12 -0400

    prep for use today

--- src/index.js
+++ src/index.js
@@ -4,37 +4,25 @@ var table;
 
 // exposing loadData to FileMaker Script
 window.loadData = function (json) {
-  var obj = JSON.parse(json); // data from FM is a string
-  var data = obj.data;
-  // return;
-  // create column headers from data
-  var firstRecord = data[0];
-  var columns = [
-    { data: "fieldData.Id", title: "ID", visible: false },
-    { data: "fieldData.City", title: "City" },
-    { data: "fieldData.CompanyName", title: "Company" },
-    { data: "fieldData.State", title: "State" },
-    { data: "fieldData.StreetAddress", title: "Address" },
-  ];
 
-  console.log(columns);
+// TODO: Load the DataTables libraries by linking to the DataTables CDN.
+  //TODO: Get data from FileMaker
+//TODO: prep it for the JS.
+  
+  
+  //TODO: create the columns.
+
+
+  //TODO: Explore options of Datatables
+  //https://datatables.net/reference/option/
+  //https://datatables.net/examples/index
   // Create the DataTable, after destroying it if already exists
-  if (table) table.destroy();
   table = $("#dtable").DataTable({
     paging: true,
     pageLength: 20,
     searching: true,
     colReorder: true,
-    columns: columns,
-    data: data,
+    columns: [],
+    data: [],
   });
 
-  // Add the click handler to the row, after removing it if already exists
-  $("#dtable tbody").off("dblclick");
-  $("#dtable tbody").on("dblclick", "tr", function () {
-    var record = table.row(this).data();
-    var json = JSON.stringify(record);
-
-    FileMaker.PerformScript("On Double Click Row", json);
-  });
-};
